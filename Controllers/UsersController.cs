e8f3d1767bc7e7dda939ca8bdda379d30b0a2a18
Kibreab Gizatu <kibreabg@gmail.com>
2021-07-19 12:09:55 +0300

    URL dev vs prod now in settings file

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -12,159 +12,186 @@ using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using ZcraPortal.Data;
 using ZcraPortal.Dtos;
 using ZcraPortal.Model;
 
-namespace ZcraPortal.Controllers {
-    [Route ("api/[controller]")]
+namespace ZcraPortal.Controllers
+{
+    [Route("api/[controller]")]
     [ApiController]
-    public class UsersController : ControllerBase {
+    public class UsersController : ControllerBase
+    {
         private readonly IZcraPortalRepo _repository;
+
         private readonly JWTSettings _jwtsettings;
         private readonly IMapper _mapper;
-        private string developmentString = "http://localhost:5000/api/Users/Login";
-        private string productionString = "http://portal.zcra.com/zcrabackend/api/Users/Login";
+        private IConfiguration _configuration;
+        private string issuer = "";
 
-        public UsersController (IZcraPortalRepo repository, IOptions<JWTSettings> jwtsettings, IMapper mapper) {
+        public UsersController(IZcraPortalRepo repository, IOptions<JWTSettings> jwtsettings, IMapper mapper, IConfiguration configuration)
+        {
             _repository = repository;
             _jwtsettings = jwtsettings.Value;
             _mapper = mapper;
+            _configuration = configuration;
         }
 
         // GET: api/Users
         [HttpGet]
-        public ActionResult<IEnumerable<Users>> GetUsers () {
-            var allUsers = _repository.GetAll<Users> ();
-            return Ok (allUsers);
+        public ActionResult<IEnumerable<Users>> GetUsers()
+        {
+            var allUsers = _repository.GetAll<Users>();
+            return Ok(allUsers);
         }
 
         // GET: api/Users/5
-        [HttpGet ("{id}", Name = "GetUser")]
-        public ActionResult<Users> GetUser (int id) {
-            var theUser = _repository.GetFirst<Users> (x => x.Id == id);
-            if (theUser != null) {
-                return Ok (theUser);
-            } else {
-                return NotFound ();
+        [HttpGet("{id}", Name = "GetUser")]
+        public ActionResult<Users> GetUser(int id)
+        {
+            var theUser = _repository.GetFirst<Users>(x => x.Id == id);
+            if (theUser != null)
+            {
+                return Ok(theUser);
+            }
+            else
+            {
+                return NotFound();
             }
         }
 
         // PUT: api/Users/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for
         // more details see https://aka.ms/RazorPagesCRUD.
-        [HttpPut ("{id}")]
-        public ActionResult<UserUpdateDto> UpdateUser (int id, UserUpdateDto userUpdateDto) {
-            var userFromRepo = _repository.GetFirst<Users> (x => x.Id == id);
-            if (userFromRepo == null) {
-                return NotFound ();
+        [HttpPut("{id}")]
+        public ActionResult<UserUpdateDto> UpdateUser(int id, UserUpdateDto userUpdateDto)
+        {
+            var userFromRepo = _repository.GetFirst<Users>(x => x.Id == id);
+            if (userFromRepo == null)
+            {
+                return NotFound();
             }
-            _mapper.Map (userUpdateDto, userFromRepo);
+            _mapper.Map(userUpdateDto, userFromRepo);
 
-            _repository.Update<Users> (userFromRepo);
-            _repository.SaveChanges ();
+            _repository.Update<Users>(userFromRepo);
+            _repository.SaveChanges();
 
-            return NoContent ();
+            return NoContent();
         }
 
         // POST: api/Users
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for
         // more details see https://aka.ms/RazorPagesCRUD.
         [HttpPost]
-        public ActionResult<UserReadDto> CreateUser (UserCreateDto userCreateDto) {
-            var userModel = _mapper.Map<Users> (userCreateDto);
-            _repository.Create<Users> (userModel);
-            _repository.SaveChanges ();
+        public ActionResult<UserReadDto> CreateUser(UserCreateDto userCreateDto)
+        {
+            var userModel = _mapper.Map<Users>(userCreateDto);
+            _repository.Create<Users>(userModel);
+            _repository.SaveChanges();
 
-            var userReadDto = _mapper.Map<UserReadDto> (userModel);
+            var userReadDto = _mapper.Map<UserReadDto>(userModel);
 
-            return CreatedAtRoute (nameof (GetUser), new { id = userReadDto.Id }, userReadDto);
+            return CreatedAtRoute(nameof(GetUser), new { id = userReadDto.Id }, userReadDto);
         }
 
         // DELETE: api/Users/5
-        [HttpDelete ("{id}")]
-        public ActionResult<Users> DeleteUser (int id) {
-            var userFromRepo = _repository.GetFirst<Users> (x => x.Id == id);
-            if (userFromRepo == null) {
-                return NotFound ();
+        [HttpDelete("{id}")]
+        public ActionResult<Users> DeleteUser(int id)
+        {
+            var userFromRepo = _repository.GetFirst<Users>(x => x.Id == id);
+            if (userFromRepo == null)
+            {
+                return NotFound();
             }
 
-            _repository.Delete<Users> (userFromRepo);
-            _repository.SaveChanges ();
+            _repository.Delete<Users>(userFromRepo);
+            _repository.SaveChanges();
 
-            return NoContent ();
+            return NoContent();
         }
 
-        private bool UserExists (int id) {
-            var userFromRepo = _repository.GetFirst<Users> (x => x.Id == id);
-            if (userFromRepo == null) {
+        private bool UserExists(int id)
+        {
+            var userFromRepo = _repository.GetFirst<Users>(x => x.Id == id);
+            if (userFromRepo == null)
+            {
                 return false;
-            } else
+            }
+            else
                 return true;
         }
 
         // POST: api/Users
-        [HttpPost ("Login")]
-        public ActionResult<UserWithToken> Login ([FromBody] Users user) {
-            user = _repository.GetFirst<Users> (x => x.Username == user.Username && x.Password == user.Password);
+        [HttpPost("Login")]
+        public ActionResult<UserWithToken> Login([FromBody] Users user)
+        {
+            user = _repository.GetFirst<Users>(x => x.Username == user.Username && x.Password == user.Password);
             UserWithToken userWithToken = null;
 
-            if (user != null) {
-                RefreshToken refreshToken = GenerateRefreshToken ();
+            if (user != null)
+            {
+                RefreshToken refreshToken = GenerateRefreshToken();
 
-                user.RefreshTokens.Add (refreshToken);
-                _repository.SaveChanges ();
+                user.RefreshTokens.Add(refreshToken);
+                _repository.SaveChanges();
 
-                userWithToken = new UserWithToken (user);
+                userWithToken = new UserWithToken(user);
                 userWithToken.RefreshToken = refreshToken.Token;
             }
 
-            if (userWithToken == null) {
-                return NotFound ();
+            if (userWithToken == null)
+            {
+                return NotFound();
             }
 
             //sign your token here..
-            userWithToken.AccessToken = GenerateAccessToken (user.Id);
+            userWithToken.AccessToken = GenerateAccessToken(user.Id);
             return userWithToken;
         }
 
         // POST: api/Users
-        [HttpPost ("RegisterUser")]
-        public ActionResult<UserWithToken> RegisterUser ([FromBody] Users user) {
+        [HttpPost("RegisterUser")]
+        public ActionResult<UserWithToken> RegisterUser([FromBody] Users user)
+        {
 
-            _repository.Create<Users> (user);
-            _repository.SaveChanges ();
+            _repository.Create<Users>(user);
+            _repository.SaveChanges();
 
             UserWithToken userWithToken = null;
 
-            if (user != null) {
-                RefreshToken refreshToken = GenerateRefreshToken ();
-                user.RefreshTokens.Add (refreshToken);
-                _repository.SaveChanges ();
+            if (user != null)
+            {
+                RefreshToken refreshToken = GenerateRefreshToken();
+                user.RefreshTokens.Add(refreshToken);
+                _repository.SaveChanges();
 
-                userWithToken = new UserWithToken (user);
+                userWithToken = new UserWithToken(user);
                 userWithToken.RefreshToken = refreshToken.Token;
             }
 
-            if (userWithToken == null) {
-                return NotFound ();
+            if (userWithToken == null)
+            {
+                return NotFound();
             }
 
             //sign your token here here..
-            userWithToken.AccessToken = GenerateAccessToken (user.Id);
+            userWithToken.AccessToken = GenerateAccessToken(user.Id);
             return userWithToken;
         }
 
         // GET: api/Users
-        [HttpPost ("RefreshToken")]
-        public ActionResult<UserWithToken> RefreshToken ([FromBody] RefreshRequest refreshRequest) {
-            Users user = GetUserFromAccessToken (refreshRequest.AccessToken);
+        [HttpPost("RefreshToken")]
+        public ActionResult<UserWithToken> RefreshToken([FromBody] RefreshRequest refreshRequest)
+        {
+            Users user = GetUserFromAccessToken(refreshRequest.AccessToken);
 
-            if (user != null && ValidateRefreshToken (user, refreshRequest.RefreshToken)) {
-                UserWithToken userWithToken = new UserWithToken (user);
-                userWithToken.AccessToken = GenerateAccessToken (user.Id);
+            if (user != null && ValidateRefreshToken(user, refreshRequest.RefreshToken))
+            {
+                UserWithToken userWithToken = new UserWithToken(user);
+                userWithToken.AccessToken = GenerateAccessToken(user.Id);
 
                 return userWithToken;
             }
@@ -173,85 +200,101 @@ namespace ZcraPortal.Controllers {
         }
 
         // GET: api/Users
-        [HttpPost ("GetUserByAccessToken")]
-        public ActionResult<Users> GetUserByAccessToken ([FromBody] string accessToken) {
-            Users user = GetUserFromAccessToken (accessToken);
+        [HttpPost("GetUserByAccessToken")]
+        public ActionResult<Users> GetUserByAccessToken([FromBody] string accessToken)
+        {
+            Users user = GetUserFromAccessToken(accessToken);
 
-            if (user != null) {
+            if (user != null)
+            {
                 return user;
             }
 
             return null;
         }
 
-        private bool ValidateRefreshToken (Users user, string refreshToken) {
+        private bool ValidateRefreshToken(Users user, string refreshToken)
+        {
 
-            RefreshToken refreshTokenUser = _repository.GetFirst<RefreshToken> (x => x.Token == refreshToken);
+            RefreshToken refreshTokenUser = _repository.GetFirst<RefreshToken>(x => x.Token == refreshToken);
 
             if (refreshTokenUser != null && refreshTokenUser.UserId == user.Id &&
-                refreshTokenUser.ExpiryDate > DateTime.UtcNow) {
+                refreshTokenUser.ExpiryDate > DateTime.UtcNow)
+            {
                 return true;
             }
 
             return false;
         }
 
-        private Users GetUserFromAccessToken (string accessToken) {
-            try {
-                var tokenHandler = new JwtSecurityTokenHandler ();
-                var key = Encoding.ASCII.GetBytes (_jwtsettings.SecretKey);
+        private Users GetUserFromAccessToken(string accessToken)
+        {
+            try
+            {
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var key = Encoding.ASCII.GetBytes(_jwtsettings.SecretKey);
 
-                var tokenValidationParameters = new TokenValidationParameters {
+                var tokenValidationParameters = new TokenValidationParameters
+                {
                     ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey (key),
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
                     ValidateIssuer = false,
                     ValidateAudience = false
                 };
 
                 SecurityToken securityToken;
-                var principle = tokenHandler.ValidateToken (accessToken, tokenValidationParameters, out securityToken);
+                var principle = tokenHandler.ValidateToken(accessToken, tokenValidationParameters, out securityToken);
 
                 JwtSecurityToken jwtSecurityToken = securityToken as JwtSecurityToken;
 
-                if (jwtSecurityToken != null && jwtSecurityToken.Header.Alg.Equals (SecurityAlgorithms.HmacSha256, StringComparison.InvariantCultureIgnoreCase)) {
-                    var userId = principle.FindFirst (ClaimTypes.Name)?.Value;
+                if (jwtSecurityToken != null && jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    var userId = principle.FindFirst(ClaimTypes.Name)?.Value;
 
-                    return _repository.GetFirst<Users> (x => x.Id == Convert.ToInt32 (userId));
+                    return _repository.GetFirst<Users>(x => x.Id == Convert.ToInt32(userId));
                 }
-            } catch (Exception) {
-                return new Users ();
+            }
+            catch (Exception)
+            {
+                return new Users();
             }
 
-            return new Users ();
+            return new Users();
         }
 
-        private RefreshToken GenerateRefreshToken () {
-            RefreshToken refreshToken = new RefreshToken ();
+        private RefreshToken GenerateRefreshToken()
+        {
+            RefreshToken refreshToken = new RefreshToken();
 
             var randomNumber = new byte[32];
-            using (var rng = RandomNumberGenerator.Create ()) {
-                rng.GetBytes (randomNumber);
-                refreshToken.Token = Convert.ToBase64String (randomNumber);
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(randomNumber);
+                refreshToken.Token = Convert.ToBase64String(randomNumber);
             }
-            refreshToken.ExpiryDate = DateTime.UtcNow.AddMonths (6);
+            refreshToken.ExpiryDate = DateTime.UtcNow.AddMonths(6);
 
             return refreshToken;
         }
 
-        private string GenerateAccessToken (int userId) {
-            var tokenHandler = new JwtSecurityTokenHandler ();
-            var key = Encoding.ASCII.GetBytes (_jwtsettings.SecretKey);
-            var tokenDescriptor = new SecurityTokenDescriptor {
-                Subject = new ClaimsIdentity (new Claim[] {
+        private string GenerateAccessToken(int userId)
+        {
+            issuer = _configuration["IssuerUrl"].ToString();
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_jwtsettings.SecretKey);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[] {
                 new Claim (ClaimTypes.Name, Convert.ToString (userId))
                 }),
-                Issuer = developmentString,
-                Expires = DateTime.UtcNow.AddDays (1),
-                SigningCredentials = new SigningCredentials (new SymmetricSecurityKey (key),
+                Issuer = issuer,
+                Expires = DateTime.UtcNow.AddDays(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
                 SecurityAlgorithms.HmacSha256Signature)
             };
-            var token = tokenHandler.CreateToken (tokenDescriptor);
-            return tokenHandler.WriteToken (token);
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
         }
     }
 }
